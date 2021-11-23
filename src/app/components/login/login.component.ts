f5892e96845b3d8d62795d0e609af4bd16c1850e
BitzyG <gv.itzelandrea@gmail.com>
2021-11-23 14:25:05 -0500

    Add login function, save token

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/service/api.service';
+// import { Observable, throwError } from 'rxjs';
+// import { catchError, retry } from 'rxjs/operators';
+// import { Users } from 'src/app/components/login/headers'
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+// import { HttpContext, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,33 +14,52 @@ import { ApiService } from 'src/app/service/api.service';
 })
 export class LoginComponent implements OnInit {
 
-  // user={
-  //   email: '',
-  //   password: '',
-  //   phone: '',
-  //   code: '',
-  // }
 
-  user: any = [];
+  logInForm = new FormGroup({
+    user: new FormControl('',Validators.required),
+    password : new FormControl('',Validators.required)
+  })
+  // user: Users[] | any = [];
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, public router: Router,
+  //  headers?: HttpHeaders, context?: HttpContext,  params?: HttpParams
+  ) { }
 
   ngOnInit(): void {
-    this.userLogn()
+    // this.userLogin()
+    this.login()
   }
 
-  userLogn() {
-    this.api.userAPI()
-      .subscribe(data => {
-        console.log(data);
+  login(){
+    this.api.loginUser(this.logInForm.value).subscribe(data =>{
+      console.log(JSON.stringify(data))
+      localStorage.setItem('token', JSON.stringify(data)),
+      this.router.navigateByUrl('home');
+    })
+  }
+
+  aVer(){
+    console.log(this.logInForm.value);
+  }
+  // userLogin() {
+  //   this.api.userLogin()
+  //     .subscribe(resp => {
+  //       console.log(resp);
+  //       const keys = resp.headers.keys();
+  //       this.user = keys.map(key =>
+  //         `${key}: ${resp.headers.get(key)}`);
         // for (const d of (data as any)) {
-        //   this.user.push({
-        //     username: d.name,
+          // this.user.push({
+            // username: d.name,
         //     price: d.price
-        //   });
+          // });
         // }
-      });
-  }
+  //     });
+  // }
 
+
+
+  //               username: "mor_2314",
+  //               password: "83r5^_"
 }
 
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,18 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+// import { Observable, throwError } from 'rxjs';
+// import { catchError, retry } from 'rxjs/operators';
+// import { fromFetch } from 'rxjs/fetch';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
+  private localUrl = 'https://fakestoreapi.com/auth/login';
+
   constructor(private http: HttpClient) { }
 
-  userAPI() {
-    const localUrl = 'https://fakestoreapi.com/users';
-    return this.http.get(localUrl);
-  }
+  // userAPI() {
+  //   return this.http.get(this.localUrl);
+  // }
 
+  // userAPI() : Observable<HttpResponse<Users[]>> {
+  //   return this.http.get<Users[]>(
+  //     this.localUrl, { observe: 'response' });
+  // }
+  loginUser(user:any){
+    return this.http.post(this.localUrl, {
+      username: user.user,
+      password: user.password
+    });
+  }
 }
