bfada70f356b7d9fbfb938c66e812bcffe52158c
BitzyG <gv.itzelandrea@gmail.com>
2021-11-22 23:06:29 -0500

    config to get user API, create home component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-login',
@@ -7,16 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  user={
-    email: '',
-    password: '',
-    phone: '',
-    code: '',
-  }
+  // user={
+  //   email: '',
+  //   password: '',
+  //   phone: '',
+  //   code: '',
+  // }
 
-  constructor() { }
+  user: any = [];
+
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
+    this.userLogn()
+  }
+
+  userLogn() {
+    this.api.userAPI()
+      .subscribe(data => {
+        console.log(data);
+        // for (const d of (data as any)) {
+        //   this.user.push({
+        //     username: d.name,
+        //     price: d.price
+        //   });
+        // }
+      });
   }
 
 }
